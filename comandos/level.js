35a18b023f5bd791fb90e7b17dbf2b49d4a1f6fb
StarLindo <murilopaschoini21@gmail.com>
2021-03-22 12:15:53 +0000

    Ohayo senpai!

--- comandos/level.js
+++ comandos/level.js
@@ -9,12 +9,19 @@ pessoa = message.author
 }
 
 let levelzin2 = db.fetch(`levelzin_${pessoa.id}_${message.guild.id}`)
+if(!levelzin2){
+levelzin2 = 0
+}
 let xpzin2 = db.fetch(`xpzin_${pessoa.id}_${message.guild.id}`)
+if(!xpzin2){
+xpzin2 = 0
+}
 const embed = new Discord.MessageEmbed()
 .setTitle(`Level!`)
 .setDescription(`Level de ${pessoa}: **${levelzin2}**
 Xp de ${pessoa}: **${xpzin2}**
 Quanto de xp falta Para ${pessoa} upar de nivel: **${100 - xpzin2}**
 `)
+.setColor("#10b040")
 message.quote(embed)
 }
